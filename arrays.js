2fd856574c8bfd381447d572d869f2d416edf4ca
JuglrdQuan Bessee <ikaquanbessee@yahoo.com>
2017-11-10 01:59:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,35 @@
-
 var chocolateBars = ['snickers',"hundred grand","kitkat", "skittles"]
+
 
 function addElementToBeginningOfArray(array,element) {
   return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
-  return [element,...array]
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array,element) {
-  return [array,...element]
+  return [...array,element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
  }
+ function accessElementInArray(array, index) {
+ return array[index]
+ }
+
+function destructivelyRemoveElementFromBeginningOfArray (chocolate,vinalla,raspberry) {
+array.shift()
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+
+}
+
+function removeElementFromEndOfArray (chocolate,vinalla,raspberry) {
+  return icecreams.slice (0, icecreams.length -1)
+}
